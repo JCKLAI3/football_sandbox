932cdd70b07ad001d3c92a52079b6ffbc1557760
James Lai <jameslai@Jamess-MacBook-Pro.local>
2022-11-22 14:50:15 +0000

    Add argument to function clean_football_data to load additional data from football-data.

--- src/football_data/etl/clean.py
+++ src/football_data/etl/clean.py
@@ -2,8 +2,13 @@
 
 import pandas as pd
 
+from src.football_data.config.football_data_config import (
+    ADDITIONAL_FOOTBALL_DATA_COLUMNS,
+    MAIN_FOOTBALL_DATA_COLUMNS,
+)
 
-def clean_football_data(football_data_df, season_name):
+
+def clean_football_data(football_data_df, season_name, include_additional_columns=False):
     """Function used to clean football data grabbed from football-data
 
     Args:
@@ -36,24 +41,10 @@ def clean_football_data(football_data_df, season_name):
         )
     )
 
-    cleaned_football_data_df = cleaned_football_data_df[
-        [
-            "league_code",
-            "season_name",
-            "date",
-            "time",
-            "kickoff",
-            "hometeam",
-            "awayteam",
-            "fthg",
-            "ftag",
-            "ftr",
-            "hthg",
-            "htag",
-            "htr",
-            "b365h",
-            "b365d",
-            "b365a",
-        ]
-    ]
+    if include_additional_columns:
+        columns_of_interest = MAIN_FOOTBALL_DATA_COLUMNS + ADDITIONAL_FOOTBALL_DATA_COLUMNS
+    else:
+        columns_of_interest = MAIN_FOOTBALL_DATA_COLUMNS
+
+    cleaned_football_data_df = cleaned_football_data_df[columns_of_interest]
     return cleaned_football_data_df
